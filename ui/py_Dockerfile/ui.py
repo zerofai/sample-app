98fb8bbdaaf6f9bd60116b1c60afd514d2109e92
Disney Cheng <zerofai@gmail.com>
2024-02-01 14:40:52 +0800

    update app to include architecture diagram

--- ui/py_Dockerfile/ui.py
+++ ui/py_Dockerfile/ui.py
@@ -9,6 +9,8 @@ IMAGE_HOSTNAME = os.getenv('IMAGE_HOSTNAME')  # Read Image API URL from environm
 
 app = Flask(__name__, template_folder=template_dir)
 
+service_na = {'error': 'Backend service not available'}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -16,26 +18,44 @@ def index():
 @app.route('/greeting', methods=['GET'])
 def get_greeting():
     greeting_api_url = f"http://{GREETING_HOSTNAME}/v1/greeting"
-    greeting_response = requests.get(greeting_api_url)
-    greeting_response.raise_for_status()  # Raise an exception for non-200 status codes
-    greeting = greeting_response.json()
-    return greeting
+    try:
+        greeting_response = requests.get(greeting_api_url)
+        if greeting_response.status_code == 200:
+            greeting_response.raise_for_status()  # Raise an exception for non-200 status codes
+            greeting = greeting_response.json()
+            return jsonify(greeting)
+        else:
+            return jsonify(service_na)
+    except requests.exceptions.RequestException as e:
+        return jsonify(service_na)
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
     weather_api_url = f"http://{WEATHER_HOSTNAME}/v1/flw"
-    weather_response = requests.get(weather_api_url)
-    weather_response.raise_for_status()  # Raise an exception for non-200 status codes
-    weather = weather_response.json()
-    return weather
+    try:
+        weather_response = requests.get(weather_api_url)
+        if weather_response.status_code == 200:
+            weather_response.raise_for_status()  # Raise an exception for non-200 status codes
+            weather = weather_response.json()
+            return weather
+        else:
+            return jsonify(service_na)
+    except requests.exceptions.RequestException:
+        return jsonify(service_na)
 
 @app.route('/image', methods=['GET'])
 def get_image():
     image_api_url = f"http://{IMAGE_HOSTNAME}/v1/image"
-    image_response = requests.get(image_api_url)
-    image_response.raise_for_status()  # Raise an exception for non-200 status codes
-    image = image_response.content
-    return image
+    try:
+        image_response = requests.get(image_api_url)
+        if image_response.status_code == 200:
+            image_response.raise_for_status()  # Raise an exception for non-200 status codes
+            image = image_response.content
+            return image
+        else:
+            return jsonify(service_na)
+    except requests.exceptions.RequestException:
+        return jsonify(service_na)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
